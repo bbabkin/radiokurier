1bf17c3c4838e6d48759370f0055b7a4abf5660c
Boris <boris@BBMAC.local>
2015-10-08 16:21:07 -0400

    playerdone

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 	audioWrap.style.visibility = "visible";
 	audioWrap.className = "animated fadeInDown";
 	audioSection.html(audio);
-	audioSection.trigger('play');
+	audioSection.play();
 	return false;
     });
     $('#player-close').click(function(){

eb7d31dc3b1016a1ecbb1dcb891aa6ee36b4ba15
Boris <boris@BBMAC.local>
2015-10-08 14:34:26 -0400

    logo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008000529) do
+ActiveRecord::Schema.define(version: 20151008172807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20151008000529) do
     t.datetime "updated_at",  null: false
     t.string   "title"
     t.string   "image"
+    t.string   "permaogg"
   end
 
   create_table "banners", force: :cascade do |t|
@@ -63,6 +64,7 @@ ActiveRecord::Schema.define(version: 20151008000529) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "position"
+    t.string   "permaogg"
   end
 
 end
